9cf0be413412537a42a1a6f5fe83c6f7c010c277
Andrew James Wuelfing <awuelfing@gmail.com>
2023-01-31 17:40:59 -0500

    Add HamQTH lookup to DxWs

---
+++ DXws/Controllers/HamQTHController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DXLib;
+using DXLib.HamQTH;
+
+namespace DXws.Controllers
+{
+    [Route("api/lookups/HamQTH")]
+    public class HamQTHController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get(string callsign)
+        {
+            HamQTHGeo hamQTHGeo = new();
+            HamQTHResult hamQTHResult = await hamQTHGeo.GetGeo(callsign);
+            return Ok(hamQTHResult.SearchResult);
+        }
+    }
+}
