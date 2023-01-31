1a19c7c3ff495ae47021134d098e94b396e3bd31
Andrew James Wuelfing <awuelfing@gmail.com>
2023-01-31 11:22:55 -0500

    Initial commit of new data source implementation.  Orphaned a console app for testing

---
+++ DXLib/HamQTH/HamQTHGeo.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace DXLib.HamQTH
+{
+    public class HamQTHGeo
+    {
+        private static readonly string _username = "";
+        private static readonly string _password = "";
+        private static string _sessionID = string.Empty; // todo lock to make thread safe
+        private static DateTime _sessionIssued = DateTime.MinValue;
+        private static readonly HttpClient _httpClient;
+        private static readonly string _loginRegex = @"\<session_id\>(\w+)\<\/session_id\>";
+        private readonly NameValueCollection _workingQS;
+        private readonly XmlSerializer _xmlSerializer;
+        private static SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1);
+
+
+        static HamQTHGeo()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://www.hamqth.com/");
+
+
+        }
+        public HamQTHGeo()
+        {
+            _xmlSerializer = new XmlSerializer(typeof(HamQTHResult));
+            _workingQS = HttpUtility.ParseQueryString(string.Empty);
+        }
+
+        private async Task DoLogin()
+        {
+            _workingQS.Clear();
+            _workingQS["u"] = _username;
+            _workingQS["p"] = _password;
+            string result = await _httpClient.GetStringAsync("/xml.php?" + _workingQS.ToString());
+
+            Match match = Regex.Match(result ?? "", _loginRegex);
+            if (match.Success)
+            {
+                _sessionID = match.Groups[1].Value;
+                Debug.WriteLine($"Login success, session id is {_sessionID}");
+                _sessionIssued = DateTime.Now;
+            }
+            else
+            {
+                throw new Exception("Login failed.");
+            }
+        }
+
+        private bool CheckSessionExpiration()
+        {
+            if ((DateTime.Now - _sessionIssued).TotalMinutes > 59)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task CheckSession()
+        {
+            if(CheckSessionExpiration())
+            {
+                Debug.WriteLine("Session is old, locking");
+                await _semaphoreSlim.WaitAsync();
+                if(CheckSessionExpiration())
+                {
+                    Debug.WriteLine("Locked session, logging in");
+                    await DoLogin();
+                }
+                _semaphoreSlim.Release();
+            }
+        }
+        public async Task<HamQTHResult> GetGeo(string callsign)
+        {
+            await CheckSession();
+
+            _workingQS.Clear();
+            _workingQS["id"] = _sessionID;
+            _workingQS["callsign"] = callsign;
+            _workingQS["prg"] = "IONOSPHERE";
+
+            string result = await _httpClient.GetStringAsync("xml.php?" + _workingQS.ToString());
+            
+            if (string.IsNullOrEmpty(result)) throw new Exception("Communication failure");
+            using StringReader stringReader = new StringReader(result);
+            HamQTHResult? hamQTHResult = (HamQTHResult?)_xmlSerializer.Deserialize(stringReader);
+            if (hamQTHResult == null) throw new Exception("Failed to deserialize:\r\n" + result);
+            return hamQTHResult;
+        }
+    }
+}
---
+++ DXLib/HamQTH/HamQTHResult.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DXLib.HamQTH
+{
+    [XmlRootAttribute("HamQTH", Namespace = "https://www.hamqth.com")]
+    public class HamQTHResult
+    {
+        [XmlElement("search")]
+        public HamQTHSearchResult? SearchResult { get; set; }
+    }
+    public class HamQTHSearchResult
+    {
+        public string? callsign { get; set; }
+        public string? nick { get; set; }
+        public string? qth { get; set; }
+        public string? country { get; set; }
+        public string? adif { get; set; }
+        public string? itu { get; set; }
+        public string? CQ { get; set; }
+        public string? grid { get; set; }
+        public string? adr_name { get; set; }
+        public string? adr_street1 { get; set; }
+        public string? adr_street2 { get; set; }
+        public string? adr_street3 { get; set; }
+        public string? adr_city { get; set; }
+        public string? adr_zip { get; set; }
+        public string? adr_country { get; set; }
+        public string? adr_adif { get; set; }
+        public string? district { get; set; }
+        public string? us_state { get; set; }
+        public string? us_county { get; set; }
+        public string? oblast { get; set; }
+        public string? dok { get; set; }
+        public string? iota { get; set; }
+        public string? qsl_via { get; set; }
+        public string? lotw { get; set; }
+        public string? eqsl { get; set; }
+        public string? qsl { get; set; }
+        public string? qsldirect { get; set; }
+        public string? email { get; set; }
+        public string? jabber { get; set; }
+        public string? icq { get; set; }
+        public string? msn { get; set; }
+        public string? skype { get; set; }
+        public string? birth_year { get; set; }
+        public string? lic_year { get; set; }
+        public string? picture { get; set; }
+        public string? latitude { get; set; }
+        public string? longitude { get; set; }
+        public string? continent { get; set; }
+        public string? utc_offset { get; set; }
+        public string? facebook { get; set; }
+        public string? twitter { get; set; }
+        public string? gplus { get; set; }
+        public string? youtube { get; set; }
+        public string? linkedin { get; set; }
+        public string? flicker { get; set; }
+        public string? vimeo { get; set; }
+
+    }
+}
