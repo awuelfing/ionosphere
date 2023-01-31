f307b0779b8624e67de3e81b836c1fa20e5b4cc3
Andrew James Wuelfing <awuelfing@gmail.com>
2023-01-31 18:19:50 -0500

    DxWs manually serialize json to remove nulls

--- DXws/Controllers/HamQTHController.cs
+++ DXws/Controllers/HamQTHController.cs
@@ -1,18 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using DXLib;
 using DXLib.HamQTH;
+using System.Text.Json;
 
 namespace DXws.Controllers
 {
     [Route("api/lookups/HamQTH")]
     public class HamQTHController : Controller
     {
+        public HamQTHController()
+        {
+            
+        }
         [HttpGet]
         public async Task<ActionResult> Get(string callsign)
         {
             HamQTHGeo hamQTHGeo = new();
             HamQTHResult hamQTHResult = await hamQTHGeo.GetGeo(callsign);
-            return Ok(hamQTHResult.SearchResult);
+            string serialized = JsonSerializer.Serialize(hamQTHResult, new JsonSerializerOptions()
+            {
+                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+            });
+            return Ok(serialized);
         }
     }
 }
