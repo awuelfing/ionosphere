e4ed79fabdb52922a61660e14a696082f6666b07
Andrew James Wuelfing <awuelfing@gmail.com>
2023-02-07 11:11:09 -0500

    added Maidenhead 'from' to DxLib, and tests

---
+++ DXLib/Maidenhead/MaidenheadLocator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DXLib.Maidenhead
+{
+    public class MaidenheadLocator
+    {
+        public static readonly string MaidenheadValidationRegex = @"[A-R]{2}\d{2}([A-X]{2})?(\d{2})?$";
+        //apparently both Perl and Python have a function of this name
+        public static int Ord(char input)
+        {
+            return (int)input;
+        }
+        public static bool ValidateMaidenhead(string input)
+        {
+            var result = Regex.Match(input, MaidenheadValidationRegex);
+            return result.Success;
+        }
+        public static (double latitude, double longitude) FromMaidenhead(string input)
+        {
+            string ml = input.ToUpper().Trim();
+            // todo revisit this way of handling it once i've used it a bit
+            if(!ValidateMaidenhead(ml)) throw new Exception("Invalid maidenhead");
+
+            double lon = -180.0;
+            double lat = -90.0;
+
+            lon += (Ord(ml[0]) - Ord('A')) * 20;
+            lat += (Ord(ml[1]) - Ord('A')) * 10;
+
+            if(ml.Length >= 4)
+            {
+                lon += int.Parse(ml[2].ToString()) * 2;
+                lat += int.Parse(ml[3].ToString()) * 1;
+            }
+            if(ml.Length >= 6)
+            {
+                lon += (Ord(ml[4]) - Ord('A')) * 5.0 / 60;
+                lat += (Ord(ml[5]) - Ord('A')) * 2.5 / 60;
+            }
+            if (ml.Length == 8)
+            {
+                lon += int.Parse(ml[6].ToString()) * 5.0/600;
+                lat += int.Parse(ml[7].ToString()) * 2.5/600;
+            }
+
+            //up to here is the southwest corner
+
+            if(ml.Length == 2)
+            {
+                lon += 20 / 2;
+                lat += 10 / 2;
+            }
+            else if(ml.Length == 4)
+            {
+                lon += 2 / 2;
+                lat += 1.0 / 2;
+            }
+            else if (ml.Length == 6)
+            {
+                lon += 5.0 / 60 / 2;
+                lat += 2.5 / 60 / 2;
+            }
+            else if (ml.Length == 8)
+            {
+                lon += 5.0 / 600 / 2;
+                lat += 2.5 / 600 / 2;
+            }
+
+            //now this is the center of the grid
+            return (lat, lon);
+        }
+    }
+}
---
+++ DxLib.Tests/MaidenheadTests.cs
@@ -0,0 +1,62 @@
+﻿using DXLib.CtyDat;
+using DXLib.Maidenhead;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DxLib.Tests
+{
+    [TestClass]
+    public class MaidenheadTests
+    {
+        [TestMethod]
+        [DataRow("FN42")]
+        public void Maidenhead_Check_From_Valid_Four(string input)
+        {
+            var expectedResult = (42.5, -71);
+            var result = MaidenheadLocator.FromMaidenhead(input);
+            Assert.AreEqual(expectedResult, result);
+        }
+        [TestMethod]
+        [DataRow("FN42ew")]
+        public void Maidenhead_Check_From_Valid_Six(string input)
+        {
+            var expectedResult = (42.9375, -71.625);
+            var result = MaidenheadLocator.FromMaidenhead(input);
+            Assert.AreEqual(expectedResult, result);
+        }
+        [TestMethod]
+        [DataRow("FN42ew33")]
+        public void Maidenhead_Check_From_Valid_Eight(string input)
+        {
+            var expectedResult = (42.93125, -71.6375);
+            var result = MaidenheadLocator.FromMaidenhead(input);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [DataRow("ASD")]
+        [DataRow("FN42E")]
+        [DataRow("FN42E433")]
+        [DataRow("FN42EW3")]
+        [DataRow("VN42EW")]
+        public void Maidenhead_Check_From_Invalid(string input)
+        {
+            //awkward but AAA
+            Exception? e = null;
+            try
+            {
+                MaidenheadLocator.FromMaidenhead(input);
+            }
+            catch (Exception ex)
+            {
+                e = ex;
+            }
+            Assert.IsNotNull(e);
+        }
+
+    }
+}
+
