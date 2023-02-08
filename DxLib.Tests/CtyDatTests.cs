9a815adc876c60d1d2b27b97f93f01026a230a82
Andrew James Wuelfing <awuelfing@gmail.com>
2023-02-08 05:52:50 -0500

    Organized tests

--- DxLib.Tests/CtyDatTests.cs
+++ DxLib.Tests/CtyDatTests.cs
@@ -5,26 +5,8 @@ using DXLib.ScpDb;
 namespace DxLib.Tests
 {
     [TestClass]
-    public class DxLibTests
+    public class CtyDatTests
     {
-        [TestMethod]
-        [DataRow("AJ1AJ")]
-        [DataRow("W1AW")]
-        public void MasterScp_CheckPresent(string callsign)
-        {
-            bool result = Scp.MatchScp(callsign);
-            Assert.IsTrue(result,$"{callsign} should be present in MasterScp");
-        }
-
-        [TestMethod]
-        [DataRow("AAAAAA111NOTAMATCH")]
-        [DataRow("123123ZZZ")]
-        public void MasterScp_CheckAbsent(string callsign)
-        {
-            bool result = Scp.MatchScp(callsign);
-            Assert.IsFalse(result,$"{callsign} should not be present in MasterScp");
-        }
-
         [TestMethod]
         [DataRow("AJ1AJ")]
         [DataRow("W1AW")]
---
+++ DxLib.Tests/ScpTests.cs
@@ -0,0 +1,32 @@
+﻿using DXLib.ScpDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DxLib.Tests
+{
+    [TestClass]
+    public class ScpTests
+    {
+        [TestMethod]
+        [DataRow("AJ1AJ")]
+        [DataRow("W1AW")]
+        public void MasterScp_CheckPresent(string callsign)
+        {
+            bool result = Scp.MatchScp(callsign);
+            Assert.IsTrue(result, $"{callsign} should be present in MasterScp");
+        }
+
+        [TestMethod]
+        [DataRow("AAAAAA111NOTAMATCH")]
+        [DataRow("123123ZZZ")]
+        public void MasterScp_CheckAbsent(string callsign)
+        {
+            bool result = Scp.MatchScp(callsign);
+            Assert.IsFalse(result, $"{callsign} should not be present in MasterScp");
+        }
+
+    }
+}
