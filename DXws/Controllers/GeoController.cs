da49f815967d6b8087b75229f5fb0212133f5836
Andrew James Wuelfing <awuelfing@gmail.com>
2023-02-08 05:49:13 -0500

    Added web service for FromMaidenhead

---
+++ DXws/Controllers/GeoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DXLib.Maidenhead;
+
+namespace DXws.Controllers
+{
+    [Route("api/lookups/geo")]
+    public class GeoController : Controller
+    {
+        [Route("FromMaidenhead")]
+        [HttpGet]
+        public IActionResult FromMaidenhead(string grid)
+        {
+            if (MaidenheadLocator.ValidateMaidenhead(grid.ToUpper()))
+            {
+                var result = MaidenheadLocator.FromMaidenhead(grid);
+                return Ok(new {Latitude = result.latitude,Longitude=result.longitude});
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
