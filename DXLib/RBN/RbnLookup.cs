5c5cc79ea2a494ecb9f1554f4546ad5d56c40a04
Andrew James Wuelfing <awuelfing@gmail.com>
2023-02-08 06:32:19 -0500

    Added RBN node lookup and tests

---
+++ DXLib/RBN/RbnLookup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXLib.RBN
+{
+    public class RbnLookup
+    {
+        public static async Task<string> GetRbnQth(string input)
+        {
+            string[] RbnFile = await File.ReadAllLinesAsync("RBN.csv");
+            string? result = RbnFile.Where(x => x.StartsWith(input)).FirstOrDefault();
+            if (result == null) { return string.Empty; }
+            return result.Split(',')[1];
+        }
+    }
+}
---
+++ DxLib.Tests/RbnTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using DXLib.RBN;
+
+namespace DxLib.Tests
+{
+    [TestClass]
+    public class RbnTests
+    {
+        [TestMethod]
+        [DataRow("KM3T", "FN42ET")]
+        [DataRow("DL1REM", "JO32PC")]
+        [DataRow("W1NT-6", "FN42LU")]
+        public async Task RbnLookup_Test_Valid_Node(string input,string expected)
+        {
+            var result = await RbnLookup.GetRbnQth(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
